fa4bdfdd3f157d80f3cabac3de4a596a3da59562
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-01-31 06:00:18 +0000

    test 31 jan

--- p1original.c
+++ p1original.c
@@ -4,7 +4,19 @@ void input(int *a,int *b)
  printf("enter two numbers\n");
  scanf("%d%d",a,b);
 }
-int add(int a,int b,int *sum);
+void add(int a,int b,int *sum)
 {
     *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+    printf("sum of %d and %d is %d",a,b,sum);
+}
+int main()
+{
+    int a,b,sum=0;
+    input(&a,&b);
+    add(a,b,&sum);
+    output(a,b,sum);
+    return 0;
 }
